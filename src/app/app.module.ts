2b5907abb36ad1574fbe167dd3010e5010433d91
Rafael <rafaelramos0401@gmail.com>
2023-04-04 17:53:38 -0300

    removed: icon import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,14 +14,12 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   imports: [
     MatGridListModule,
     MatButtonModule,
     BrowserModule,
     ReactiveFormsModule,
-    MatIconModule,
     RouterModule.forRoot([
       { path: '', component: CoffeeListComponent },
       { path: 'sobre', component: AboutComponent },
