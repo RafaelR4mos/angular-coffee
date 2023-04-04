860005af372be40b7745fd2f1163c99fc05232cc
Rafael <rafaelramos0401@gmail.com>
2023-04-04 11:20:50 -0300

    styles validation and on submit event added

--- src/app/coffee-form/coffee-form.component.ts
+++ src/app/coffee-form/coffee-form.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-coffee-form',
   templateUrl: './coffee-form.component.html',
-  styleUrls: ['./coffee-form.component.scss']
+  styleUrls: ['./coffee-form.component.scss'],
 })
 export class CoffeeFormComponent {
-
+  handleSubmit(e: SubmitEvent) {
+    e.preventDefault();
+    window.alert('Interesse registrado com sucesso!');
+  }
 }
